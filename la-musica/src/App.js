1a1f31d4965d34c0b20fb3f8b055d8ac0df83c94
Lomanfan <lfan.canada@gmail.com>
2021-09-01 23:15:08 -0600

    add Dashboard component with code

--- la-musica/src/App.js
+++ la-musica/src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login.js';
+import Dashboard from './Dashboard.js';
 
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
-  return (
-    <Login />
-  );
+  return code ? <Dashboard code={code} /> :<Login />
 }
 
 export default App;
