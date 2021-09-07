75c9caefd513d594ad8cb01414d651ed87bcfb22
Lomanfan <lfan.canada@gmail.com>
2021-09-07 01:43:17 -0600

    axios.post with refreshToken, setInterval, set states of access tokens and expires in time

--- la-musica/src/useAuth.js
+++ la-musica/src/useAuth.js
@@ -1,20 +1,49 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState();
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
+  console.log(refreshToken);
+
   useEffect(() => {
     axios
-    .post('http://localhost:8080/login', {
-      code, 
-    })
-    .then(res => {
-      console.log(res.data);
-  }, [code])
-  })
+      .post("http://localhost:8080/login", {
+        code,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, null, "/");
+      })
+      .catch(err => {
+        console.log(err, "login error");
+        window.location = "/";
+      });
+  }, [code]);
+
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:8080/refresh", {
+          refreshToken,
+        })
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch(err => {
+          console.log(err, "refresh error");
+          window.location = "/";
+        });
+    }, (expiresIn - 60) * 1000);
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 }
-
-
